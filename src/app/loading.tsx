fe5c5d982f712aa18994965cb3eb37a112c38090
Muhammad Ahsan <155831296+MAhsanOfficial@users.noreply.github.com>
2025-02-07 23:05:28 +0500

    changes

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+// import { Loader } from 'lucide-react'
+import React from 'react'
+import Loader from './loading/page'
+
+const loading = () => {
+  return (
+    <div><Loader/></div>
+  )
+}
+
+export default loading
